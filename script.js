06b337fd7773598278fd553f88a51d64765fe669
ameeriit <amritpokharel51@gmail.com>
2022-07-04 22:54:36 +0545

    commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var ball = document.querySelector('.ball');
+    var toggle = document.querySelector('.toggle')
+
+    ball.classList.toggle('active')
+    toggle.classList.toggle('change')
+}
